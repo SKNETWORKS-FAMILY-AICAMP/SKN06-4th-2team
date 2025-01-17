9ba325aefa6dd8eaff02cbee63be95a85c2a2eb1
zl-zone <clehflek@gmail.com>
2025-01-17 16:20:14 +0900

    Add files via upload
    urlpatterns에 path('account/', include('account.urls')), 경로를 추가하였습니다.

--- chat/config/urls.py
+++ chat/config/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('account/', include('account.urls')),
+    path('', TemplateView.as_view(template_name='home.html')),
 ]
 # http://127.0.0.1:8000
